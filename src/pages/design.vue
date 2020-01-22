196d3f7c226db201e429e571bcebe5211524b0e8
Henry Zhang <henryzhang9802@gmail.com>
2020-01-22 20:29:37 +0000

    Finish design

--- src/pages/design.vue
+++ src/pages/design.vue
@@ -6,9 +6,9 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-10 mx-auto text-center">
-            <h3 class="section-title">System Architecture Diagram</h3>
+            <h3 class="section-title">System Architecture Diagram 1</h3>
             <img
-              src="images/design/system architecture.png"
+              src="images/design/techarch.png"
               class="img-responsive"
               style="width: 100%;"
             />
@@ -18,12 +18,14 @@
     </section>
     <!-- /System Arthitecture Diagram-->
 
+
+
     <!--ER Diagram-->
     <section class="section">
       <div class="container">
         <div class="row">
           <div class="col-lg-10 mx-auto text-center">
-            <h3 class="section-title">Entity Relationship Diagram</h3>
+            <h3 class="section-title">System Architecture Diagram 2</h3>
             <img src="images/design/architecture.png" class="img-responsive" style="width: 100%;" />
           </div>
         </div>
@@ -43,6 +45,14 @@
         <div class="row">
           <div class="col-lg-10 mx-auto text-center">
             <h3 class="section-title">Implantation of finished functionality</h3>
+          </div>
+
+          <div class="col-md-4">
+            <img
+                    src="images/design/functionality.gif"
+                    class="img-responsive alignright"
+                    style="width: 100%;"
+            />
           </div>
 
           <div class="col-md-8">
@@ -71,30 +81,29 @@
               <p>We use Watson's Speech to Text as the client's request. Also, we use Watson's assistant to give the user the option of getting more details about the specific area(by keyword detection). For AI processing, we choose to use the IBM cloud.</p>
               <br />
               <br />
-              <h4>Storage of data</h4>
-              <p>Initially, we choose MySQL for our datastore. However, with more in-depth research, we found that the NoSQL database is more suitable for us. Then we decide to use MongoDB as our database. MongoDB is a cross-platform document-oriented database.It is document-oriented. Besides, it is built for the cloud. We need to use the IBM cloud for our back-end.</p>
-              <br />
-              <br />
-              <h4>Back-end server</h4>
-              <p>The server framework we use is the Express. It is a web application framework for Node.js.It is designed for building web applications and APIs. Using Express.js can decrease the line of code you need to write compared with plain Node.js.</p>
-              <br />
-              <br />
-              <h4>Summary of final decision</h4>
-              <p>
-                For the front-end, we use flutter SDK(Dart) and Unity for Android app and Vue.js for website service.
-                For the back-end, we use node.js for the back-end server,
-                Plus, we use IBM Watson and IBM Cloud for AI language processing.
-                For data store, we use MongoDB.
-              </p>
+
             </div>
           </div>
-            <div class="col-md-4">
-                <img
-                        src="images/design/functionality.gif"
-                        class="img-responsive alignright"
-                        style="width: 100%;"
-                />
-            </div>
+
+          <div class="col-lg-12">
+            <h4>Storage of data</h4>
+            <p>Initially, we choose MySQL for our datastore. However, with more in-depth research, we found that the NoSQL database is more suitable for us. Then we decide to use MongoDB as our database. MongoDB is a cross-platform document-oriented database.It is document-oriented. Besides, it is built for the cloud. We need to use the IBM cloud for our back-end.</p>
+            <br />
+            <br />
+            <h4>Back-end server</h4>
+            <p>The server framework we use is the Express. It is a web application framework for Node.js.It is designed for building web applications and APIs. Using Express.js can decrease the line of code you need to write compared with plain Node.js.</p>
+            <br />
+            <br />
+            <h4>Summary of final decision</h4>
+            <p>
+              For the front-end, we use flutter SDK(Dart) and Unity for Android app and Vue.js for website service.
+              For the back-end, we use node.js for the back-end server,
+              Plus, we use IBM Watson and IBM Cloud for AI language processing.
+              For data store, we use MongoDB.
+            </p>
+
+          </div>
+
         </div>
       </div>
     </section>
