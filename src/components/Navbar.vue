b6e52aab255ca4eb8f26c1b308a6ac4a7d2e7564
Henry Zhang <henryzhang9802@gmail.com>
2020-01-23 12:17:21 +0000

    Add auto collapse on mobile

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -42,10 +42,16 @@
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  watch: {
+    '$route' () {
+      document.querySelector('.navbar-collapse').classList.remove("show");
+    }
   }
 }
 </script>
