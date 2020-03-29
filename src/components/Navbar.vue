4e824d9c1cf91acfd5a237b0d3be0f5c588ec904
Henry Zhang <henryzhang9802@gmail.com>
2020-03-29 19:45:04 +0100

    Fix router

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,44 +1,4 @@
 <template>
-<!--  <header class="navigation fixed-top">-->
-<!--    <nav class="navbar navbar-expand-lg navbar-dark">-->
-<!--      <router-link class="navbar-brand font-tertiary h3" to="/" >-->
-<!--        <img src="images/logo.png" style="max-height: 60px;" alt="Team 13">-->
-<!--      </router-link>-->
-<!--      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"-->
-<!--              aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">-->
-<!--        <span class="navbar-toggler-icon"></span>-->
-<!--      </button>-->
-
-<!--      <div class="collapse navbar-collapse text-center" id="navigation">-->
-<!--        <ul class="navbar-nav ml-auto">-->
-<!--          <li class="nav-item active">-->
-<!--            <router-link class="nav-link" to="/" >Home</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link class="nav-link" to="/requirements" >Requirements</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link class="nav-link" to="/research" >Research</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link class="nav-link" to="/hci" >Hci</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link class="nav-link" to="/design" >Prototype</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link class="nav-link" to="/achievement" >Achievement</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link class="nav-link" to="/plan" >Term 2 Plan</router-link>-->
-<!--          </li>-->
-<!--          <li class="nav-item">-->
-<!--            <router-link class="nav-link" to="/appendices" >Appendices</router-link>-->
-<!--          </li>-->
-<!--        </ul>-->
-<!--      </div>-->
-<!--    </nav>-->
-<!--  </header>-->
 
   <header class="header">
     <div class="container containerHeader">
@@ -56,8 +16,8 @@
         </button>
         <div class="collapse navbar-collapse main-menu justify-content-end" id="fixedNavbar">
           <ul class="navbar-nav">
-            <li class="nav-item active">
-              <router-link class="nav-link" to="/" >Home</router-link>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/" exact>Home</router-link>
             </li>
             <li class="nav-item">
               <router-link class="nav-link" to="/requirements" >Requirements</router-link>
