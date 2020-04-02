d0844dfdea5933130935cc137be3261440a53078
Henry Zhang <henryzhang9802@gmail.com>
2020-04-02 21:41:31 +0100

    Add testing

--- src/pages/testing.vue
+++ src/pages/testing.vue
@@ -31,6 +31,7 @@
                         </p>
                         <br><br>
                         <h6>Integration Testing</h6>
+                        <img src="images/testing/1.png" class="img-responsive item-shadow" style= "width: 100%;">
                         <p>
                         Integration testing is to test how parts of the system work together.<br><br>
                         At this stage, we focus on verifying the following separate systems work together.<br><br>
@@ -46,6 +47,30 @@
                         For testing the backend API endpoint, we use Postman - simple, secure and collaborative automated testing for API to check if we get the right response from GET/PUSH request.
                         The image is the example when we use Postman to testing API endpoints.
                         </p>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-10 mx-auto text-left">
+                        <h3 class="section-title">Compatibility Testing</h3>
+                        <p>To check whether the application is capable of running on different devices, operating system or software,
+                            We tested our Android application to make sure that it works in different Android System and different Android devices.
+                            We have found the app working ok on all of our available Android 9+ devices. If the department could provide more devices we can expand our testing range.
+                            <br><br>
+                            For website application, we tested it on three browsers across three operating systems by "BrowserStack" - which is a compatibility testing and responsive testing platform for website application and mobile application. The result shows that the web application is compatible with all browsers that are ES5-compliant.
+                        </p><br><br>
+                        Here are some screenshots about testing stage.
+                        <img src="images/testing/2.png" class="img-responsive item-shadow" style= "width: 100%;">
+                        Firefox73 in Windows 10
+                        <img src="images/testing/3.png" class="img-responsive item-shadow" style= "width: 100%;">
+                        Google Chrome 82 in Windows 10
+                        <br><br>
+                        <p>The test result shows that there is no major display problem with different browsers and different operating systems.</p>
                     </div>
                 </div>
             </div>
@@ -57,7 +82,16 @@
                 <div class="row">
                     <div class="col-lg-10 mx-auto text-left">
                         <h3 class="section-title"><span>Responsive</span>Design Testing</h3>
-                        <p>PLACEHOLDER</p>
+                        <p>For responsive design testing, we can use Chrome developer tools to test this
+                            on different screen sizes in both mobile devices and PC.  Our website application has passed all tests with different screen sizes.
+                            Here are some screenshots when we tested our website application in different sizes.
+                        </p>
+                        <img src="images/testing/4.png" class="img-responsive item-shadow" style= "width: 100%;">
+                        <img src="images/testing/5.png" class="img-responsive item-shadow" style= "width: 100%;">
+                        <img src="images/testing/6.png" class="img-responsive item-shadow" style= "width: 100%;">
+                        <img src="images/testing/7.png" class="img-responsive item-shadow" style= "width: 100%;">
+                        <img src="images/testing/8.png" class="img-responsive item-shadow" style= "width: 100%;">
+                        <img src="images/testing/9.png" class="img-responsive item-shadow" style= "width: 100%;">
                     </div>
                 </div>
             </div>
@@ -101,5 +135,7 @@
 </script>
 
 <style scoped>
-
+    img{
+        margin: 30px 0;
+    }
 </style>
