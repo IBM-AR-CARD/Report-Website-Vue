802e9994796d3c7bb888eb0ae644f6cd073a0615
Henry Zhang <henryzhang9802@gmail.com>
2020-04-03 23:50:34 +0100

    Update appendices auto new tab

--- src/pages/appendices.vue
+++ src/pages/appendices.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
     <div style="height: 9vh; background: #7e57c2"></div>
-        <iframe class="embed-responsive-item" src="appendices/" style="border:none;width: 100%;height: 91vh;"></iframe>
+        <iframe ref="iframe" class="embed-responsive-item" :src="src" style="border:none;width: 100%;height: 92vh;" @load="load"></iframe>
     </div>
 </template>
 
@@ -9,6 +9,31 @@
 
     export default {
         name: "appendices",
+        data: function () {
+            return {
+                src:"appendices/",
+                lastSrc: "appendices/"
+            }
+        },
+        methods: {
+            load: function(){
+                try{
+                    if(this.$refs["iframe"].contentWindow.location.host == window.location.host){
+                        this.lastSrc = this.$refs["iframe"].contentWindow.location.href
+                    }else{
+                        // eslint-disable-next-line no-console
+                        // console.error("aaaaaa",this.lastSrc)
+                        this.src=this.lastSrc+"?"+Math.random();
+                        alert("That didn't work, please try right click and open in a new tab.")
+                    }
+                }catch (e) {
+                    // eslint-disable-next-line no-console
+                    // console.error(e,"aaaaaa",this.lastSrc)
+                    this.src=this.lastSrc+"?"+Math.random();
+                    alert("That didn't work, please try right click and open in a new tab.")
+                }
+            }
+        },
         components: {
         },
         mounted(){
